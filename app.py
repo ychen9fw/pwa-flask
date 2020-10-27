81c0ac6035e2315466e15c60ef8469be86e66801
ychen9 <ychen9@futurewei.com>
2020-10-27 13:03:51 -0700

    zip

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import build
-from flask import Flask, request
+from flask import Flask, request, send_file, send_from_directory, safe_join, abort
+
 app = Flask(__name__)
 
 
 @app.route('/build_apk', methods=['POST'])
 def build_apk():
     content = request.json
-    result = build.build(content['website'])
-    return 'Build: ' + result
+    folder = build.build(content['website'])
+    # return 'Build: ' + result
+    return send_from_directory(folder, filename="pwa.zip", as_attachment=True)
 
 
 if __name__ == "__main__":
--- build.py
+++ build.py
@@ -1,10 +1,13 @@
 import subprocess
 import time
+import os
 
 
 def build(url):
     folder = str("/tmp/pwa"+str(int(time.time()*1000)))
-    subprocess.run(["/usr/bin/pwabuilder", url, "-d", folder])
-    subprocess.run(["/usr/bin/zip", "-r", folder+"/pwa.zip", folder+"/"])
-    return folder+"/pwa.zip"
+    os.system("/usr/bin/pwabuilder " + url + " -d " + folder)
+    # subprocess.run(["/usr/bin/pwabuilder", url, "-d", folder])
+    os.system("cd " + folder + "; /usr/bin/zip -r pwa.zip *")
+    # subprocess.run(["/usr/bin/zip", "-r", folder+"/pwa.zip", folder+"/"])
+    return folder
 
