e2defc0214ebb744c3f6c87cdaf7342cba034d96
ychen9 <ychen9@futurewei.com>
2020-11-19 16:21:59 -0800

    build options

--- build.py
+++ build.py
@@ -5,9 +5,12 @@ import base64
 
 def build(pkg, url, kits, ads, agcs):
     folder = str("/tmp/pwa"+str(int(time.time()*1000)))
-    os.system("/usr/bin/pwabuilder " + url + " -d " + folder)
+    # os.system("/usr/bin/pwabuilder " + url + " -d " + folder)
+    os.system("mkdir " + folder)
     os.system("echo " + agcs + " | base64 --decode > " + folder + "/agconnect-services.json")
-    os.system("/usr/bin/node /home/ubuntu/pwa_builder/make_hms.js --package " + pkg + " --url " + url + " --output " + folder)
+    pwacmd = "/usr/bin/node /home/ubuntu/pwa_builder/make_hms.js --package " + pkg + " --url " + url + " --json " + folder + "/agconnect-services.json" + " --output " + folder
+    print(pwacmd)
+    os.system(pwacmd)
     os.system("cd " + folder + "; /usr/bin/zip -r pwa.zip *")
     return folder
 
