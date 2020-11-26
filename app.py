0b6d1259766c1ac29513d533197a6e46103022e8
ychen9 <ychen9@futurewei.com>
2020-11-25 16:06:30 -0800

    cor options

--- app.py
+++ app.py
@@ -2,30 +2,34 @@ import build
 import publish
 import time
 import os
+from flask_cors import CORS
 from flask import Flask, request, send_file, send_from_directory, safe_join, abort
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/build_apk', methods=['POST'])
 def build_apk():
     content = request.json
     print(content)
+    print(content['agcs'])
     folder = build.build(content['packageId'],
                          content['host'],
                          content['HMSKits'],
                          content['ads_id'],
-                         content['agcs'],
+                         content['agcs'].replace('data:application/json;base64,', ''),
                          content['signing']['alias'],
                          content['signing']['fullName'],
                          content['signing']['organization'],
                          content['signing']['organizationalUnit'],
                          content['signing']['countryCode'],
                          content['signing']['keyPassword'],
-                         content['signing']['storePassword']
+                         content['signing']['storePassword'],
+                         content['iconUrl']
                          )
     # return 'Build: ' + result
-    return send_from_directory(folder, filename="pwa.zip", as_attachment=True)
+    print('returning folder' + folder)
+    return send_from_directory(folder, filename="pwa.zip", as_attachment=True, mimetype='application/zip')
 
 
 @app.route('/publish_apk', methods=['POST'])
--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 import time
 import os
+import urllib.request
+import requests
 
-
-def build(pkg, url, kits, ads, agcs, signingAlias, signingFullname, signingOrganization, signingOrganizationalUnit, signingCountryCode, signingKeyPassword, signingStorePassword):
+def build(pkg, url, kits, ads, agcs, signingAlias, signingFullname, signingOrganization, signingOrganizationalUnit, signingCountryCode, signingKeyPassword, signingStorePassword, iconUrl):
     folder = str("/tmp/pwa"+str(int(time.time()*1000)))
+    icon = folder + "/ic_launcher.png"
     os.system("mkdir " + folder)
     os.system("echo " + agcs + " | base64 --decode > " + folder + "/agconnect-services.json")
     pwacmd = "/usr/bin/node /home/ubuntu/pwa_builder/make_hms.js --package " + pkg + " --url " + url + " --json " + folder + "/agconnect-services.json" + " --output " + folder
@@ -14,6 +16,12 @@ def build(pkg, url, kits, ads, agcs, signingAlias, signingFullname, signingOrgan
     pwacmd += " --signingCountryCode " + signingCountryCode
     pwacmd += " --signingKeyPassword " + signingKeyPassword
     pwacmd += " --signingStorePassword " + signingStorePassword
+    try:
+        response = requests.get(iconUrl)
+        urllib.request.urlretrieve (iconUrl, icon)
+        pwacmd += " --icon " + icon
+    except requests.ConnectionError as exception:
+        print(iconUrl + "icon url does not exist")
     os.system("echo " + pwacmd)
     os.system(pwacmd)
     os.system("cd " + folder + "; /usr/bin/zip -r pwa.zip *")
